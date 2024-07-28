4b81a435faa05c9ad259da911ba30a6f747c644d
Patchwork Horse <paintcolt@gmail.com>
2024-07-28 13:24:25 -0400

    Begin adding real tests

---
+++ test/HostFixtureTests/Extension_tests.cs
@@ -0,0 +1,19 @@
+
+namespace HostFixtureTests;
+
+public class Extension_tests
+{
+    [Fact]
+    public void should_attach_fixture_to_buidlder()
+    {
+        // Arrange
+        var targetBuilder = new HostBuilder();
+
+        // Act
+        var fixturedHost = targetBuilder.ConfigureFixture()
+            .GenerateFixturedIHost(); 
+
+        // Assert
+        Assert.NotNull(fixturedHost);
+    }
+}
